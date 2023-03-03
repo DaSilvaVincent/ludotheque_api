fc9ce45f1febe78b640338f6cacb07f32d370cf0
Theo Journee <theo_journee@ens.univ-artois.fr>
2023-03-03 10:43:37 +0100

    Ajout des fichiers migrations de Like et Achat.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(AchatSeeder::class);
+        $this->call(likeSeeder::class);
     }
 }
