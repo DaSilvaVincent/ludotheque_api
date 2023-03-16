8429ef881907e81b7cb94145483c0878bb658b89
Theo Journee <theo_journee@ens.univ-artois.fr>
2023-03-16 10:28:18 +0100

    #6 ajout de la methde login.

--- app/Http/Controllers/Api/AdherentControlleur.php
+++ app/Http/Controllers/Api/AdherentControlleur.php
@@ -5,18 +5,52 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AdherentRequest;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AdherentControlleur extends Controller
 {
 
+    public function __construct() {
+        $this->middleware('auth:api', [
+            'except' => [
+                'login',
+                'register'
+            ]
+        ]);
+    }
+
+    public function login(Request $request) {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        $essentiel = $request->only('email', 'password');
+        $token = Auth::attempt($essentiel);
+        if (!$token) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+        $user = Auth::user();
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ],200);
+    }
+
+
     public function register(AdherentRequest $request) {
         $request->validate([
             'login' => "required|string|between:5,50",
-            'email' => "required|string|between:5,50",
-            'name' => "required|string|between:5,50",
-            'password' => "required|string|between:5,50",
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
             'prenom' => "required|string|between:5,50",
             'pseudo ' => "required|string|between:5,50"
         ]);
@@ -28,7 +62,8 @@ class AdherentControlleur extends Controller
             'pseudo' => $request['pseudo'],
             'password' => Hash::make($request['password']),
         ]);
-        $token = Auth::login($user);
+        $essentiel = $request->only('email', 'password');
+        $token = Auth::attempt($essentiel);
         if (!$token) {
             return response()->json([
                 'status' => 'error',
