7cb3afb62de7897b507fea3a05d50c520bfc1214
Hanart Thomas <thomas_hanart@ens.univ-artois.fr>
2023-04-01 15:32:17 +0200

    Ajout routes avec middleware

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CommentaireController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('commentaires')->group(function () {
+    Route::post('/', [CommentaireController::class, 'create'])
+        ->middleware(['auth', 'role:adherent'])
+        ->name('salles.store');
+    Route::put('/{id}', [CommentaireController::class, 'edit'])
+        ->middleware(['auth', 'role:commentaire-moderateur'])
+        ->name('commentaires.update');
+    Route::delete('/{id}', [CommentaireController::class, 'delete'])
+        ->middleware(['auth', 'role:commentaire-moderateur'])
+        ->name('commentaires.delete');
 });
